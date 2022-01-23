fe86b0fe823fa38c366b55542845d036bf369ec6
arturyyys <arturyyys@gmail.com>
2022-01-23 12:59:23 +0200

    272. Adding Dynamic Routes with Params

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
 import MainHeader from "./components/MainHeader";
 
 function App() {
@@ -14,6 +15,9 @@ function App() {
         <Route path='/products'>
           <Products />
         </Route>
+        <Route path='/product-detail/:productID'>
+          <ProductDetail />
+        </Route>
       </main>
     </div>
   );
@@ -23,3 +27,4 @@ export default App;
 
 // our-domain.com/welcome => Welcome Component
 // our-domain.com/products => Products Component
+// our-domain.com/product-detail/<any value>
