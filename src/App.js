f832f0f681b15be070271119218f2d74cb8d0e0c
arturyyys <arturyyys@gmail.com>
2022-01-24 19:02:14 +0200

    277. Time to Practice: Onwards to a New Project

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
-import { Redirect, Route, Switch } from "react-router-dom";
-import Welcome from "./pages/Welcome";
-import Products from "./pages/Products";
-import ProductDetail from "./pages/ProductDetail";
-import MainHeader from "./components/MainHeader";
-
 function App() {
   return (
     <div>
-      <MainHeader />
-      <main>
-        <Switch>
-          <Route path='/' exact>
-            <Redirect to='/welcome' />
-          </Route>
-          <Route path='/welcome'>
-            <Welcome />
-          </Route>
-          <Route path='/products' exact>
-            <Products />
-          </Route>
-          <Route path='/products/:productID'>
-            <ProductDetail />
-          </Route>
-        </Switch>
-      </main>
+
     </div>
   );
 }
 
 export default App;
-
-// our-domain.com/welcome => Welcome Component
-// our-domain.com/products => Products Component
-// our-domain.com/product-detail/<any value>
