9eaefd2909350f872575cab27c77d79cf076e3ef
arturyyys <arturyyys@gmail.com>
2022-02-01 18:46:58 +0200

    react authentication

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
-import { Redirect, Route, Switch } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
-import AllQuotes from "./pages/AllQuotes";
-import QuoteDetail from "./pages/QuoteDetail";
-import NewQuote from "./pages/NewQuote";
-import Layout from "./components/layout/Layout";
-import NotFound from "./pages/NotFound";
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
-          <Redirect to='/quotes' />
+          <HomePage />
         </Route>
-        <Route path='/quotes' exact>
-          <AllQuotes />
+        <Route path='/auth'>
+          <AuthPage />
         </Route>
-        <Route path='/quotes/:quoteId'>
-          <QuoteDetail />
-        </Route>
-        <Route path='/new-quote'>
-          <NewQuote />
-        </Route>
-        <Route path='*'>
-          <NotFound />
+        <Route path='/profile'>
+          <UserProfile />
         </Route>
       </Switch>
     </Layout>
@@ -31,3 +24,5 @@ function App() {
 }
 
 export default App;
+
+// new
